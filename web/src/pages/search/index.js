271ea9f692aa49513d4d848b4a2623c3c1b5a977
Brian Leung <brian_yf_leung@manulifeam.com>
2019-05-04 15:53:28 +0800

    [Search] simple UI for search

--- web/src/pages/search/index.js
+++ web/src/pages/search/index.js
@@ -3,7 +3,10 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { withStyles } from '@material-ui/core/styles'
 import createMuiTheme from '../../ui/theme'
 import { getAllFeaturesFromPoint } from '../../utils/features'
-import Button from '@material-ui/core/Button'
+import Input from '@material-ui/core/Input';
+
+
+
 
 import dc2003 from '../../data/DCCA_2003'
 import dc2007 from '../../data/DCCA_2007'
@@ -21,10 +24,16 @@ const styles = theme => ({
     flexGrow: 1
   },
   toolbar: theme.mixins.toolbar,
-  yearButton: {
+  search1: {
     position: 'absolute',
-    width: 100,
-    top: '15%'
+    top: '15%',
+    width: '50%'
+  },
+  search2: {
+    position: 'absolute',
+    top: '15%',
+    left: '55%',
+    width: '45%'
   }
 })
 
@@ -34,11 +43,28 @@ class SearchPage extends Component {
   }
 
   render() {
+    const { classes } = this.props
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <main>
-          This is search page
+        <main className={classes.content}>
+        <div className={classes.container}>
+      <Input
+        defaultValue="Search Address"
+        className={classes.search1}
+        inputProps={{
+          'aria-label': 'Description',
+        }}
+      />
+
+      <Input
+        placeholder="Search People"
+        className={classes.search2}
+        inputProps={{
+          'aria-label': 'Description',
+        }}
+      />
+    </div>
         </main>
       </MuiThemeProvider>
     )
