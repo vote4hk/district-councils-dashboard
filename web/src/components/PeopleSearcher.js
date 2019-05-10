01977077d27d368370511e0e65feb595cd9a95b2
Brian Leung <brianleung1017@gmail.com>
2019-05-11 02:03:58 +0800

    [Component] Create PeopleSearcher

---
+++ web/src/components/PeopleSearcher.js
@@ -0,0 +1,182 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import deburr from 'lodash/deburr'
+import Autosuggest from 'react-autosuggest'
+import match from 'autosuggest-highlight/match'
+import parse from 'autosuggest-highlight/parse'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import MenuItem from '@material-ui/core/MenuItem'
+import Popper from '@material-ui/core/Popper'
+import { withStyles } from '@material-ui/core/styles'
+import candidates from '../data/candidates'
+
+const suggestions = candidates.map(item => {
+  return {
+    name: item.name_chi
+  }})
+
+function renderInputComponent(inputProps) {
+  const { classes, inputRef = () => {}, ref, ...other } = inputProps
+
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputRef: node => {
+          ref(node)
+          inputRef(node)
+        },
+        classes: {
+          input: classes.input,
+        },
+      }}
+      {...other}
+    />
+  )
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+  const matches = match(suggestion.name, query)
+  const parts = parse(suggestion.name, matches)
+
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) =>
+          part.highlight ? (
+            <span key={String(index)} style={{ fontWeight: 500 }}>
+              {part.text}
+            </span>
+          ) : (
+            <strong key={String(index)} style={{ fontWeight: 300 }}>
+              {part.text}
+            </strong>
+          ),
+        )}
+      </div>
+    </MenuItem>
+  )
+}
+
+function getSuggestions(value) {
+  const inputValue = deburr(value.trim()).toLowerCase()
+  const inputLength = inputValue.length
+
+  return inputLength === 0 ? [] : suggestions.filter(suggestion => suggestion.name.includes(value))
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name
+}
+
+const styles = theme => ({
+  root: {
+    height: 250,
+    flexGrow: 1,
+  },
+  container: {
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    // left: 0,
+    // right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  }
+})
+
+class PeopleSearcher extends React.Component {
+  state = {
+    single: '',
+    popper: '',
+    suggestions: [],
+  }
+
+  handleSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value),
+    })
+  }
+
+  handleSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    })
+  }
+
+  handleChange = name => (event, { newValue }) => {
+    this.setState({
+      [name]: newValue,
+    })
+  }
+
+  handleSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
+    this.props.handlePeopleSelected(suggestion)
+  }
+
+  render() {
+    const { classes } = this.props
+    
+    const autosuggestProps = {
+      renderInputComponent,
+      suggestions: this.state.suggestions,
+      onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+      onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+      onSuggestionSelected: this.handleSuggestionSelected,
+      getSuggestionValue,
+      renderSuggestion,
+    }
+
+    return (
+      <div className={this.props.class}>
+        <Autosuggest
+          {...autosuggestProps}
+          inputProps={{
+            classes,
+            label: 'Label',
+            placeholder: 'Please type a candidate name',
+            value: this.state.popper,
+            onChange: this.handleChange('popper'),
+            inputRef: node => {
+              this.popperNode = node
+            },
+            InputLabelProps: {
+              shrink: true,
+            },
+          }}
+          theme={{
+            suggestionsList: classes.suggestionsList,
+            suggestion: classes.suggestion,
+          }}
+          renderSuggestionsContainer={options => (
+            <Popper anchorEl={this.popperNode} open={Boolean(options.children)}>
+              <Paper
+                square
+                {...options.containerProps}
+                style={{ width: this.popperNode ? this.popperNode.clientWidth : null }}
+              >
+                {options.children}
+              </Paper>
+            </Popper>
+          )}
+        />
+      </div>
+    )
+  }
+}
+
+PeopleSearcher.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(PeopleSearcher)
