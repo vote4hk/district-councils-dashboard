45d36dce1dcd77ff6301e723e6a91a588df525ba
Brian Leung <brianleung1017@gmail.com>
2019-05-25 01:11:42 +0800

    Change BrowserRouter to HashRouter

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Router basename="district-councils-dashboard">
+  <Router>
     <App />
   </Router>,
   document.getElementById('root')
