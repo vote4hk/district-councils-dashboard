6e0a1ea120660076babe3bf45532b25bdbc49978
Brian Leung <brian_yf_leung@manulifeam.com>
2019-06-20 01:24:27 +0800

    add people.json

--- web/src/data/csv2json.js
+++ web/src/data/csv2json.js
@@ -1,50 +1,42 @@
 const csv = require('csvtojson')
 const fs = require('fs');
-const csvFilePath = './electors.csv'
+const csvFilePath = './people.csv'
 
 const output = []
+
+const addElection = (obj, cur) => {
+    delete obj.id
+    delete obj.name_chi
+    delete obj.gender
+    if (cur.estimated_birth !== obj.estimated_birth && !cur.estimated_birth.includes('/')) {
+        cur.estimated_birth = `${Math.min(parseInt(cur.estimated_birth),parseInt(obj.estimated_birth))}/${Math.max(parseInt(cur.estimated_birth),parseInt(obj.estimated_birth))}`
+    }
+    return obj
+}
 
 csv()
     .fromFile(csvFilePath)
     .then(jsonObj => {
         jsonObj.forEach(obj => {
-            const candidateObj = {
-                number: parseInt(obj.number),
-                cName: obj.cName,
-                cAlias: obj.cAlias,
-                eName: obj.eName,
-                eAlias: obj.eAlias,
-                cAffiliation: obj.cAffiliation,
-                eAffiliation: obj.eAffiliation,
-                vote: parseInt(obj.vote.replace(',', '')),
-                win: obj.win === 'Y' ? true : false,
-                continue: obj.continue === 'Y' ? true : false
-            }
-
-            const dccaObj = {
-                cacode: obj.cacode,
-                cname: obj.cname,
-                ename: obj.ename,
-                candidates: []
-            }
-
-            if (output.findIndex(o => parseInt(o.year) === parseInt(obj.year)) === -1) {
-                output.push({
-                    year: parseInt(obj.year),
-                    election: []
-                })
-            }
-
-
-            const yearIndex = output.findIndex(o => parseInt(o.year) === parseInt(obj.year))
-            let cacodeIndex = output[yearIndex].election.findIndex(e => e.cacode === obj.cacode)
-
-            if (cacodeIndex === -1) output[yearIndex].election.push(dccaObj)
-
-            cacodeIndex = output[yearIndex].election.findIndex(e => e.cacode === obj.cacode)
-            output[yearIndex].election[cacodeIndex].candidates.push(candidateObj)
+                // Existing People
+                const cur = output.find(o => o.name_chi === obj.name_chi && Math.abs(parseInt(o.estimated_birth) - parseInt(obj.estimated_birth)) <= 1)
+                if (cur) {
+                    cur.elections.push(addElection(obj, cur))
+                }
+                else {
+                    // New People
+                    const basic_obj = {
+                        name_chi: obj.name_chi,
+                        gender: obj.gender,
+                        estimated_birth: obj.estimated_birth,
+                        elections: [addElection(obj, obj)] // Add first election item
+                    }
+                    output.push(basic_obj)
+                } 
         }
         )
+        
         const outputJson = JSON.stringify(output, null, 2)
-        fs.writeFile('electors.json', outputJson, (err, result) => { });
+        fs.writeFile('people.json', outputJson, (err, result) => { });
+        console.log(`Number of unique people: ${output.length}`)
     })
