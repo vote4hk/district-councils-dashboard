8d1b0d37359e96fa432dc32e30144e7a6fa8fafb
Brian Leung <brianleung1017@gmail.com>
2019-06-29 01:38:16 +0800

    [District Page] Data fetch logic refactoring and UI enhancement

--- web/src/pages/district/index.js
+++ web/src/pages/district/index.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 import { styled } from '@material-ui/styles';
-import Button from '@material-ui/core/Button';
-import { withTheme } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardHeader from '@material-ui/core/CardHeader'
+import IconButton from '@material-ui/core/IconButton'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import Typography from '@material-ui/core/Typography'
-import district from '../../data/district'
-import { fetchData } from '../../utils/httpClient'
-
+import CustomizedProgressBars from '../../components/BorderLinearProgress'
+import { getDistrictDataByYearCode } from '../../service/district'
+import Avatar from '@material-ui/core/Avatar'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import OLMap from '../../components/OLMap'
 
 const Map = styled(Paper)({
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -22,124 +27,179 @@ const Map = styled(Paper)({
   color: 'white',
   height: 300,
   padding: '0 30px',
-});
+})
 
 const DistrictCard = styled(Paper)({
   background: '#f6f6f6',
-  border: 0,
   color: '#00000',
-  height: 300,
-  padding: '0 30px',
-});
+  height: 300
+})
 
 class DistrictPage extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      district: undefined
     }
   }
 
   async componentDidMount() {
-    const { match: { params } } = this.props
-    const { year, code } = params
-    const query = `
-    {
-      dc_area(where: {
-        cacode: {
-          _eq: "${code}"
-        },
-        year: {
-          _eq: "${year}"
-        }
-      }) {
-        id
-        cname
-        people (where: {
-          year: {
-            _eq: "${year}"
-          }
-        }) {
-          id
-          name_chi
-          year
-        }
-      }
+    const { match: { params: { year, code } } } = this.props
+    this.setState({
+      year,
+      code,
+      district: await getDistrictDataByYearCode(year, code)
+    })
+  }
+
+  async componentDidUpdate(prevProps) {
+    const { match: { params: { year, code } } } = this.props
+    if (year !== prevProps.match.params.year) {
+      this.setState({
+        year,
+        district: await getDistrictDataByYearCode(year, code)
+      })
     }
-    `
-    const fetched_data = await fetchData(query);
-    console.log(fetched_data)
+
+    if (code !== prevProps.match.params.code) {
+      this.setState({
+        code,
+        district: await getDistrictDataByYearCode(year, code)
+      })
+    }
+  }
+
+  handleChangeDistrict = (year, code) => {
+    if (!year || ! code) return
+    this.props.history.replace(`/district/${year}/${code}`)
   }
 
   render() {
-    const { match: { params } } = this.props
-    const { year, code } = params
-    const districtCode = code.substring(0, 1)
-    const dcca = code !== districtCode ? code : null
-    const data = district[year][districtCode][dcca];
+    const { year, code, district } = this.state
+    if (!district) return null
 
-
-
-    
-
-    
-   
     return (
-      // TODO: UI design
-      <Grid container>
-        <Grid item xs={9}>
-        <Map>Styled Components</Map>
-        </Grid>
-        <Grid item xs={3}>
-        <DistrictCard>
-              <CardHeader
-                title={`${data.code} ${data.name}`}
-                subheader={year}
-              />
-              <CardContent>
-              </CardContent>
-        </DistrictCard>
-        </Grid>
-        <Grid item xs={12}>
-        <Typography variant="subtitle1" gutterBottom>
-                  主要屋邨 / 地區：
-                </Typography>
-                <List>
-                  {
-                    data.mainArea.map((area, index) => <ListItem alignItems="flex-start" key={index}>
-                      <ListItemText
-                        primary={`${Object.keys(area)[0]}. ${area[Object.keys(area)[0]]}`}
-                      />
-                    </ListItem>)
+      <React.Fragment>
+        <Container maxWidth='lg'>
+          <Grid container spacing={3}>
+            <Grid item xs={8}>
+              <OLMap
+                year={year}
+                code={code}
+                changeDistrict={this.handleChangeDistrict}
+                 />
+            </Grid>
+            <Grid item xs={4}>
+              <DistrictCard>
+                <Box
+                  p={1} 
+                  border={0}
+                  color='primary.minor'>
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    alignItems='center'
+                    justifyContent='space-between'>
+                    <IconButton aria-label='arrow_back'>
+                      <ArrowBackIcon />
+                    </IconButton>
+                    <Typography variant='button' gutterBottom>
+                      {year}
+                    </Typography>
+                    <IconButton aria-label='arrow_forward'>
+                      <ArrowForwardIcon />
+                    </IconButton>
+                  </Box>
 
-                  }
-                </List>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper>
-                <Typography variant="subtitle1" gutterBottom>
-                  估計人口： {data.expectedPopulation}
-                </Typography>
-
-                
-                
-          </Paper>
-        </Grid>
-        {/* <Grid item xs={6}>
-          <Paper>
+                  <Typography variant='h4' color='inherit' style={{ display: 'inline-block' }}>
+                    {district.name_zh}
+                  </Typography>
+                  <Typography variant='h4' color='inherit' style={{ display: 'inline-block' }}>
+                    {code}
+                  </Typography>
+                  <Divider />
+                  <List>
+                    <ListItemText primary={'區議員'} />
+                    <ListItemSecondaryAction>{district.councillor.person.name_zh}</ListItemSecondaryAction>
+                  </List>
+                  <List>
+                    <ListItemText primary={'政黨'} />
+                    <ListItemSecondaryAction>{district.councillor.political_affiliation ? district.councillor.political_affiliation.name_zh : '-'}</ListItemSecondaryAction>
+                  </List>
+                </Box>
+              </DistrictCard>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant='h6' gutterBottom>主要屋邨 / 地區：</Typography>
             <List>
               {
-                data.boundaries.map((boundary, index) => <ListItem alignItems="flex-start" key={index}>
+                district.main_areas.map((area, index) => <ListItem alignItems='flex-start' key={index}>
                   <ListItemText
-                    primary={`${Object.keys(boundary)[0]} ${boundary[Object.keys(boundary)[0]]}`}
+                    primary={`${Object.keys(area)[0]}. ${area[Object.keys(area)[0]]}`}
                   />
                 </ListItem>)
-
               }
             </List>
-          </Paper>
-        </Grid> */}
-      </Grid>
+          </Grid>
+        </Container>
+        <Grid item xs={12}>
+          <Container maxWidth='lg'>
+            <Typography variant='h5' gutterBottom>估計人口</Typography>
+            {district.expected_population}
+            
+            <Typography variant='h5' gutterBottom>議員候選人</Typography>
+            <Grid item xs={12}>
+              {
+                district.candidates.sort((a, b) => a.candidate_number - b.candidate_number).map(candidate =>
+                  <div 
+                    style={{ width: '100%' }}
+                    key={candidate.candidate_number}>
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent='space-between'>
+                    <Box p={1}>
+                      <Avatar
+                        src={`/static/images/avatar/${year}/${code}_${year === 2011 ? '0' : ''}${candidate.candidate_number}.jpg`}
+                        imgProps={{ onError: (e) => { e.target.src = '/static/images/avatar/default.png'; } }} >
+                      </Avatar>
+                    </Box>
+                    <Box p={1}>
+                      <Typography gutterBottom variant='h6'>
+                        {`${candidate.candidate_number}. ${candidate.person.name_zh}`}
+                      </Typography>
+                      </Box>
+                    <Box p={1}>
+                      <Typography color='textSecondary' variant='body2'>
+                        陣營
+                      </Typography>
+                      <Typography gutterBottom variant='body1'>
+                        {candidate.political_affiliation ? candidate.political_affiliation.name_zh: ''}
+                      </Typography>
+                      </Box>
+                    <Box p={1}>
+                      <Typography gutterBottom variant='body1'>
+                        {`${candidate.votes} (${candidate.vote_percentage}%)`}
+                      </Typography>
+                    </Box>
+                    <Box p={1}>
+                      <Typography color='textSecondary' variant='body2'>
+                        得票率
+                      </Typography>
+                      <CustomizedProgressBars
+                        value={parseFloat(candidate.vote_percentage)} />
+                    </Box>
+                    <Box p={1}>
+                      {candidate.won && <CheckCircleIcon />}
+                    </Box>
+                  </Box>
+                  </div>)
+              }
+            </Grid>
+          </Container>
+        </Grid>
+      </React.Fragment>
     )
   }
 }
