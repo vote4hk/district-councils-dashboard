831551bb5e75d6a9ad4d5e01e9ce084d3e932fc5
Brian Leung <brianleung1017@gmail.com>
2019-06-29 01:33:54 +0800

    Create BorderLinearProgress component

---
+++ web/src/components/BorderLinearProgress.js
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import { lighten, makeStyles, withStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+const BorderLinearProgress = withStyles({
+    root: {
+        height: 10,
+        backgroundColor: lighten('#ff6c5c', 0.5),
+    },
+    bar: {
+        borderRadius: 20,
+        backgroundColor: '#ff6c5c',
+    },
+})(LinearProgress)
+
+export default function CustomizedProgressBars(props) {
+    const classes = useStyles()
+
+    return (
+        <React.Fragment>
+            <BorderLinearProgress
+                className={classes.margin}
+                variant="determinate"
+                color="secondary"
+                value={props.value}
+            />
+        </React.Fragment>
+    );
+}
