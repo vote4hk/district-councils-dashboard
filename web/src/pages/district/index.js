9afaa3d12277e8c326c117e4d366e571449a9910
Nandi Wong <nandi0315@gmail.com>
2019-07-06 00:43:12 +0800

    refak(distrcit): refactor the district page (#13)
    * refactor a bit the district page
    
    * extract the main area
    
    * tidy up the codes

--- web/src/pages/district/index.js
+++ web/src/pages/district/index.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react'
-import { styled } from '@material-ui/styles'
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import IconButton from '@material-ui/core/IconButton'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
-import CheckCircleIcon from '@material-ui/icons/CheckCircle'
-import List from '@material-ui/core/List'
-import ListItemText from '@material-ui/core/ListItemText'
-import Divider from '@material-ui/core/Divider'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import Typography from '@material-ui/core/Typography'
-import CustomizedProgressBars from '../../components/BorderLinearProgress'
-import Avatar from '@material-ui/core/Avatar'
 import Box from '@material-ui/core/Box'
 import OLMap from '../../components/OLMap'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
-
-const DistrictCard = styled(Paper)({
-  background: '#f6f6f6',
-  color: '#00000',
-  height: 300,
-})
+import DistrictCard from 'components/district/DistrictCard'
+import MainAreas from 'components/district/MainAreas'
+import CandidateList from 'components/district/CandidateList'
+import styled from 'styled-components'
+import { bps } from 'utils/responsive'
 
 const GET_DISTRICTS = gql`
   query($year: Int!, $code: String!) {
@@ -48,9 +33,39 @@ const GET_DISTRICTS = gql`
   }
 `
 
-class DistrictPage extends Component {
-  homeUrl = 'https://cswbrian.github.io/district-councils-dashboard/'
+const FullWidthBox = styled(Box)`
+  && {
+    width: 100%;
+  }
+`
 
+const LowerBackgroundContainer = styled(Box)`
+  && {
+    width: 100vw;
+    position: relative;
+    margin-left: -50vw;
+    left: 50%;
+    background-color: #fafafa;
+  }
+`
+
+const FlexRowContainer = styled(Box)`
+  && {
+    display: flex;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    ${bps.up('md')} {
+      width: 100%;
+    }
+
+    ${bps.up('lg')} {
+      width: 1440px;
+    }
+    margin: auto;
+  }
+`
+
+class DistrictPage extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     //  if (this.props.route.path === nextProps.route.path) return false;
     return true
@@ -79,240 +94,74 @@ class DistrictPage extends Component {
     this.props.history.push(`/district/${parseInt(year, 10) + 4}/${code}`)
   }
 
-  renderPrevElectionButton(year) {
-    return year > 1999 ? (
-      <IconButton
-        aria-label="arrow_back"
-        onClick={this.onPrevElection.bind(this)}
-      >
-        <ArrowBackIcon />
-      </IconButton>
-    ) : (
-      // if there is no next button, show a 48x48 empty box to align the 2 elements on the right
-      <div style={{ width: '48px', height: '48px' }}></div>
-    )
-  }
-
-  renderNextElectionButton(nextElectionYear, currentYear) {
-    return nextElectionYear < currentYear ? (
-      <IconButton
-        aria-label="arrow_forward"
-        onClick={this.onNextElection.bind(this)}
-      >
-        <ArrowForwardIcon />
-      </IconButton>
-    ) : (
-      // if there is no next button, show a 48x48 empty box to align the 2 elements on the left
-      <div style={{ width: '48px', height: '48px' }}></div>
-    )
-  }
-
   render() {
     const {
       match: {
         params: { year, code },
       },
     } = this.props
-    const nextElectionYear = parseInt(year, 10) + 4
-    const currentYear = new Date().getFullYear()
 
     return (
-      <Box display="flex" flexWrap="wrap" alignContent="flex-start">
-        <Box
-          width={{ sm: '100%', md: '960px' }}
-          height={{ sm: '300px', md: '400px' }}
-        >
-          <OLMap
-            year={year}
-            code={code}
-            changeDistrict={this.handleChangeDistrict}
-          />
-        </Box>
-        <Query query={GET_DISTRICTS} variables={{ year, code }}>
-          {({ loading, error, data }) => {
-            if (loading) return null
-            if (error) return `Error! ${error}`
-            const district = data.dc_constituencies[0]
+      <>
+        <FlexRowContainer>
+          <Box
+            width={{ sm: '100%', md: '960px' }}
+            height={{ sm: '300px', md: '400px' }}
+          >
+            <OLMap
+              year={year}
+              code={code}
+              changeDistrict={this.handleChangeDistrict}
+            />
+          </Box>
+          <Query query={GET_DISTRICTS} variables={{ year, code }}>
+            {({ loading, error, data }) => {
+              if (loading) return null
+              if (error) return `Error! ${error}`
+              const district = data.dc_constituencies[0]
 
-            return (
-              <>
-                <Box
-                  p={1}
-                  width={{ sm: '100%', md: '400px' }}
-                  height={{ sm: '300px', md: '400px' }}
-                >
-                  <DistrictCard>
-                    <Box p={1} border={0} color="primary.minor">
-                      <Box
-                        display="flex"
-                        flexDirection="row"
-                        alignItems="center"
-                        justifyContent="space-between"
-                      >
-                        {this.renderPrevElectionButton(year)}
-                        <Typography variant="button" gutterBottom>
-                          {year}
+              return (
+                <>
+                  <Box
+                    p={0}
+                    paddingLeft="30px"
+                    width={{ sm: '100%', md: '400px' }}
+                    height={{ sm: '300px', md: '400px' }}
+                  >
+                    <DistrictCard
+                      {...district}
+                      year={parseInt(year, 10)}
+                      code={code}
+                      onNextElection={this.onNextElection.bind(this)}
+                      onPrevElection={this.onPrevElection.bind(this)}
+                    />
+                  </Box>
+                  <FullWidthBox>
+                    <MainAreas areas={district.main_areas || []} />
+                  </FullWidthBox>
+                  <LowerBackgroundContainer>
+                    <FlexRowContainer>
+                      <FullWidthBox>
+                        <Typography variant="h5" gutterBottom>
+                          估計人口
                         </Typography>
-                        {this.renderNextElectionButton(
-                          nextElectionYear,
-                          currentYear
-                        )}
-                      </Box>
-                      <Typography
-                        variant="h4"
-                        color="inherit"
-                        style={{ display: 'inline-block' }}
-                      >
-                        {district.name_zh}
-                      </Typography>
-                      <Typography
-                        variant="h4"
-                        color="inherit"
-                        style={{ display: 'inline-block' }}
-                      >
-                        {code}
-                      </Typography>
-                      <Divider />
-                      <List>
-                        <ListItemText primary={'區議員'} />
-                        <ListItemSecondaryAction></ListItemSecondaryAction>
-                      </List>
-                      <List>
-                        <ListItemText primary={'政黨'} />
-                        <ListItemSecondaryAction></ListItemSecondaryAction>
-                      </List>
-                    </Box>
-                  </DistrictCard>
-                </Box>
-
-                <Grid item xs={12}>
-                  {district.main_areas.length > 0 && (
-                    <Grid item xs={12}>
-                      <Container maxWidth="lg">
-                        <Typography variant="h6" gutterBottom>
-                          主要屋邨 / 地區
-                        </Typography>
-                        <Box
-                          display="flex"
-                          flexWrap="wrap"
-                          alignContent="flex-start"
-                        >
-                          {district.main_areas.map((area, index) => (
-                            <Box mr={1} key={index}>
-                              {`${Object.keys(area)[0]}. ${
-                                area[Object.keys(area)[0]]
-                              }`}
-                            </Box>
-                          ))}
-                        </Box>
-                      </Container>
-                    </Grid>
-                  )}
-                  <Grid item xs={12}>
-                    <Container maxWidth="lg">
-                      <Typography variant="h5" gutterBottom>
-                        估計人口
-                      </Typography>
-                      {district.expected_population}
-
-                      <Typography variant="h5" gutterBottom>
-                        議員候選人
-                      </Typography>
-                      <Grid item xs={12}>
-                        {district.candidates
-                          .sort(
-                            (a, b) => a.candidate_number - b.candidate_number
-                          )
-                          .map(candidate => (
-                            <div
-                              style={{ width: '100%' }}
-                              key={candidate.candidate_number}
-                            >
-                              <Box
-                                display="flex"
-                                flexDirection="row"
-                                justifyContent="space-between"
-                              >
-                                <Box p={1}>
-                                  <Avatar
-                                    src={`${
-                                      this.homeUrl
-                                    }/static/images/avatar/${year}/${code}_${
-                                      year === 2011 ? '0' : ''
-                                    }${candidate.candidate_number}.jpg`}
-                                    imgProps={{
-                                      onError: e => {
-                                        e.target.src =
-                                          this.homeUrl +
-                                          '/static/images/avatar/default.png'
-                                      },
-                                    }}
-                                  ></Avatar>
-                                </Box>
-                                <Box p={1}>
-                                  <Typography gutterBottom variant="h6">
-                                    {`${
-                                      candidate.candidate_number == null
-                                        ? ''
-                                        : candidate.candidate_number + '.'
-                                    } ${candidate.person.name_zh ||
-                                      candidate.person.name_en}`}
-                                  </Typography>
-                                </Box>
-                                <Box p={1}>
-                                  <Typography
-                                    color="textSecondary"
-                                    variant="body2"
-                                  >
-                                    陣營
-                                  </Typography>
-                                  <Typography gutterBottom variant="body1">
-                                    {candidate.political_affiliation
-                                      ? candidate.political_affiliation.name_zh
-                                      : ''}
-                                  </Typography>
-                                </Box>
-                                <Box p={1}>
-                                  <Typography gutterBottom variant="body1">
-                                    {`${candidate.votes} (${candidate.vote_percentage}%)`}
-                                  </Typography>
-                                </Box>
-                                <Box p={1}>
-                                  <Typography
-                                    color="textSecondary"
-                                    variant="body2"
-                                  >
-                                    得票率
-                                  </Typography>
-                                  <CustomizedProgressBars
-                                    value={parseFloat(
-                                      candidate.vote_percentage
-                                    )}
-                                  />
-                                </Box>
-                                <Box p={1}>
-                                  {candidate.is_won && <CheckCircleIcon />}
-                                  {!candidate.is_won && (
-                                    <div
-                                      style={{
-                                        width: '24px',
-                                        height: '24px',
-                                      }}
-                                    ></div>
-                                  )}
-                                </Box>
-                              </Box>
-                            </div>
-                          ))}
-                      </Grid>
-                    </Container>
-                  </Grid>
-                </Grid>
-              </>
-            )
-          }}
-        </Query>
-      </Box>
+                        {district.expected_population}
+                      </FullWidthBox>
+                      <FullWidthBox>
+                        <CandidateList
+                          candidates={district.candidates}
+                          year={parseInt(year, 10)}
+                          code={code}
+                        />
+                      </FullWidthBox>
+                    </FlexRowContainer>
+                  </LowerBackgroundContainer>
+                </>
+              )
+            }}
+          </Query>
+        </FlexRowContainer>
+      </>
     )
   }
 }
