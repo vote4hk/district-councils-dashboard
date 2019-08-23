49d09ce582bcb29dd1bae4567a623dbf71358f76
Brian Leung <brianleung1017@gmail.com>
2019-08-24 00:00:11 +0800

    Add CouncillorSelection component

---
+++ web/src/components/district/CouncillorSelection.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { PlainDialog } from '../molecules/Dialog'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import { PlainCard } from '../molecules/Card'
+import AddIcon from '@material-ui/icons/Add'
+
+export default function CustomizedDialogs() {
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <PlainCard onClick={handleClickOpen}>
+        <Box display="flex">
+          <Box flexGrow={1}>
+            <Typography variant="h6" gutterBottom>
+              現任區議員
+            </Typography>
+
+            <Typography variant="h6">
+              此為新增選區，按址選擇現任區議員
+            </Typography>
+          </Box>
+          <Box>
+            <AddIcon style={{ fontSize: 48 }} />
+          </Box>
+        </Box>
+      </PlainCard>
+      <PlainDialog
+        open={open}
+        handleClose={handleClose}
+        title="此選區於2019年設立"
+        content="content"
+        actions="actions"
+      ></PlainDialog>
+    </>
+  )
+}
