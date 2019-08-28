2179796b20ffb3fc8f283f9367858ebb980eaa7c
gayaugayau <54344332+gayaugayau@users.noreply.github.com>
2019-08-28 22:27:27 +0800

    Switch to use BrowserRouter with spa-github-pages (#36)
    * Switch to use BrowserRouter with spa-github-pages
    
    * Add 404 page

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 import './index.css'
