87da0a63e83d194b17362afda9ae39d2fd1d819b
Nandiheath <nandi0315@gmail.com>
2019-09-27 00:47:46 +0800

    npm audit fix

--- web/src/components/atoms/Slider/index.js
+++ web/src/components/atoms/Slider/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import Slider from '@material-ui/core/Slider'
 
 export default props => {
-  return <Slider aria-label="custom thumb label" {...props} />
+  return <></>
 }
