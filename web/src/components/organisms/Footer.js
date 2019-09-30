a4a04471e4758ab9fad528bfe90a74387462b566
gayaugayau <gayaugayau@protonmail.com>
2019-09-30 16:12:58 +0800

    Fix unexpected horizontal scroll

--- web/src/components/organisms/Footer.js
+++ web/src/components/organisms/Footer.js
@@ -16,6 +16,10 @@ const StyledFooter = styled(Box)`
   }
 `
 
+const StyledTypography = styled(Typography)`
+  padding: 0 16px;
+`
+
 const StyledDivider = styled(Divider)`
   && {
     margin-top: 16px;
@@ -33,6 +37,7 @@ const LinkBox = styled(Box)`
     margin-left: 16px;
     margin-top: 16px;
     word-break: keep-all;
+    overflow: hidden;
   }
 `
 
@@ -40,16 +45,16 @@ function Footer(props) {
   return (
     <>
       <StyledFooter>
-        <Typography variant="body2" gutterBottom>
+        <StyledTypography variant="body2" gutterBottom>
           本網站所刊載資訊全為公開資料，歸納自選舉管理委員會丶選舉事務處丶政府統計處丶各區區議會網站及
           <a href="https://github.com/initiummedia/hk_district_council_election">
             端傳媒
           </a>
           ，刊載前已盡力確保資料真確性，如有建議或錯漏，請按下方連結回報。
-        </Typography>
-        <Typography variant="body2">
+        </StyledTypography>
+        <StyledTypography variant="body2">
           本網站與任何2019年區議會選舉候選人或其助選成員無關，刊載資料目的非為促使或阻礙任何候選人在選舉中當選。
-        </Typography>
+        </StyledTypography>
         <StyledDivider />
         <Columns>
           <LinkBox>
