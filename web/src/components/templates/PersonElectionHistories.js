1761d06b30800a35875f13f930510d14a0562ff0
Brian Leung <brian_yf_leung@manulifeam.com>
2019-10-05 18:01:26 +0800

    Person election history card clickable for 2019

--- web/src/components/templates/PersonElectionHistories.js
+++ web/src/components/templates/PersonElectionHistories.js
@@ -4,6 +4,7 @@ import { PlainCard } from 'components/molecules/Card'
 import Typography from '@material-ui/core/Typography'
 import { Box, Grid } from '@material-ui/core'
 import { SuccessText, FailureText } from '../atoms/Text'
+import { UnstyledNavLink } from '../atoms/UnstyledLink'
 
 const PersonElectionHistories = props => {
   const { histories } = props
@@ -11,53 +12,58 @@ const PersonElectionHistories = props => {
   return (
     <>
       {histories.map((m, index) => (
-        <PlainCard key={index}>
-          <Box>
-            <Typography variant="h6" gutterBottom>
-              {m.year}
-            </Typography>
-          </Box>
-          <Grid container>
-            <Grid item xs={4}>
-              <Typography variant="h5">{m.constituency.name_zh}</Typography>
-            </Grid>
-            <Grid item xs={5}>
-              <Typography variant="h5">{`${m.political_affiliation || '-'}（${
-                m.camp
-              }）`}</Typography>
-            </Grid>
-            {m.votes > 0 && (
-              <Grid item xs={3}>
-                {m.is_won ? (
-                  <SuccessText>
-                    {m.votes > 0 ? `${m.votes}票` : '自動當選'}
-                  </SuccessText>
-                ) : (
-                  <FailureText>{`${m.votes}票`}</FailureText>
-                )}
+        <UnstyledNavLink
+          key={index}
+          to={m.year === 2019 && `/district/2019/${m.constituency.code}`}
+        >
+          <PlainCard>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                {m.year}
+              </Typography>
+            </Box>
+            <Grid container>
+              <Grid item xs={4}>
+                <Typography variant="h5">{m.constituency.name_zh}</Typography>
               </Grid>
-            )}
-          </Grid>
-          <Grid container>
-            <Grid item xs={4}>
-              <Typography variant="h6">{m.constituency.code}</Typography>
-            </Grid>
-            <Grid item xs={5}>
-              <Typography variant="h6">報稱政治聯繫</Typography>
-            </Grid>
-            {m.votes > 0 ? (
-              <Grid item xs={3}>
-                {m.is_won ? (
-                  <SuccessText>當選</SuccessText>
-                ) : (
-                  <FailureText>落敗</FailureText>
-                )}
+              <Grid item xs={5}>
+                <Typography variant="h5">{`${m.political_affiliation || '-'}（${
+                  m.camp
+                }）`}</Typography>
               </Grid>
-            ) : (
-              <Grid item xs={3}></Grid>
-            )}
-          </Grid>
-        </PlainCard>
+              {m.votes > 0 && (
+                <Grid item xs={3}>
+                  {m.is_won ? (
+                    <SuccessText>
+                      {m.votes > 0 ? `${m.votes}票` : '自動當選'}
+                    </SuccessText>
+                  ) : (
+                    <FailureText>{`${m.votes}票`}</FailureText>
+                  )}
+                </Grid>
+              )}
+            </Grid>
+            <Grid container>
+              <Grid item xs={4}>
+                <Typography variant="h6">{m.constituency.code}</Typography>
+              </Grid>
+              <Grid item xs={5}>
+                <Typography variant="h6">報稱政治聯繫</Typography>
+              </Grid>
+              {m.votes > 0 ? (
+                <Grid item xs={3}>
+                  {m.is_won ? (
+                    <SuccessText>當選</SuccessText>
+                  ) : (
+                    <FailureText>落敗</FailureText>
+                  )}
+                </Grid>
+              ) : (
+                <Grid item xs={3}></Grid>
+              )}
+            </Grid>
+          </PlainCard>
+        </UnstyledNavLink>
       ))}
     </>
   )
