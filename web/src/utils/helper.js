fb755989c389885be0cd7f3f9772f50cf5b1c6be
coolsunwind <brianleung1017@gmail.com>
2019-10-07 23:03:57 +0800

    fix camp color

--- web/src/utils/helper.js
+++ web/src/utils/helper.js
@@ -112,6 +112,7 @@ export const getColorFromCamp = camp => {
   if (!camp) return 'uncertain'
   const mapping = {
     泛民: 'democracy',
+    民主: 'democracy',
     建制: 'establishment',
     本土: 'localist',
     傘兵: 'localist',
