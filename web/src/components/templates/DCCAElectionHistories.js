b58a156a3a81e4a42bc2794b1fb813568a6d8a67
coolsunwind <brianleung1017@gmail.com>
2019-10-13 02:56:26 +0800

    Enhance election history logic

--- web/src/components/templates/DCCAElectionHistories.js
+++ web/src/components/templates/DCCAElectionHistories.js
@@ -4,6 +4,13 @@ import styled from 'styled-components'
 import ScrollableTabs from 'components/organisms/ScrollableTabs'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import { Typography } from '@material-ui/core'
+import DCCAElectionResult from 'components/templates/DCCAElectionResult'
+import Box from '@material-ui/core/Box'
+import { COLORS } from 'ui/theme'
+import Divider from '@material-ui/core/Divider'
+
+import { getColorFromCamp } from 'utils/helper'
 
 const GET_DCCA_ELECTION_HISTORIES = gql`
   query($year: Int!, $code: String!) {
@@ -20,8 +27,20 @@ const GET_DCCA_ELECTION_HISTORIES = gql`
         votes
         vote_percentage
         is_won
+        political_affiliation
       }
     }
+  }
+`
+const DCCAElectionResultContainer = styled(Box)`
+  && {
+    padding: 0 16px;
+  }
+`
+
+const StyledTab = styled.div`
+  && {
+    color: ${props => COLORS.camp[props.camp].background};
   }
 `
 
@@ -31,34 +50,16 @@ class DCCAElectionHistories extends Component {
     this.state = {}
   }
 
-  // console.log(histories)
-
-  // const result = histories.map(history => <Query query={GET_DCCA_ELECTION_HISTORIES} variables={{ year: history.year, code: history.CACODE }}>
-  //   {({ loading, error, data }) => {
-  //     if (loading) return null
-  //     if (error) return `Error! ${error}`
-  //     console.log(data)
-  //     return data
-  //   }}
-  // </Query>)
-
-  // console.log(result)
-
   render() {
     const { histories } = this.props
 
+    const filteredHistories = histories.filter(
+      history => history.year !== '2019'
+    )
+
     return (
       <ScrollableTabs
-        titles={histories.map(history => {
-          return (
-            <div>
-              {history.CACODE}
-              <br /> {history.year}
-            </div>
-          )
-        })}
-      >
-        {histories.map(history => (
+        titles={filteredHistories.map(history => (
           <Query
             query={GET_DCCA_ELECTION_HISTORIES}
             variables={{ year: history.year, code: history.CACODE }}
@@ -67,15 +68,41 @@ class DCCAElectionHistories extends Component {
               if (loading) return null
               if (error) return `Error! ${error}`
 
-              const dccaResult = data.dcd_constituencies[0]
+              const electionResult = data.dcd_constituencies[0]
 
-              // this.setState({
-              //   electionHistory: {
-              //     [history.year] : dccaResult
-              //   }
-              // })
+              return (
+                <StyledTab
+                  camp={getColorFromCamp(
+                    electionResult.candidates.find(candi => candi.is_won).camp
+                  )}
+                >
+                  <Typography variant="h6">
+                    {electionResult.candidates.find(candi => candi.is_won).camp}
+                  </Typography>
+                  <Typography variant="h6">{history.year}</Typography>
+                  <Typography variant="h6">{electionResult.name_zh}</Typography>
+                </StyledTab>
+              )
+            }}
+          </Query>
+        ))}
+      >
+        {filteredHistories.map(history => (
+          <Query
+            query={GET_DCCA_ELECTION_HISTORIES}
+            variables={{ year: history.year, code: history.CACODE }}
+          >
+            {({ loading, error, data }) => {
+              if (loading) return null
+              if (error) return `Error! ${error}`
 
-              return <>{dccaResult.name_zh}</>
+              const electionResult = data.dcd_constituencies[0]
+
+              return (
+                <DCCAElectionResultContainer>
+                  <DCCAElectionResult candidates={electionResult.candidates} />
+                </DCCAElectionResultContainer>
+              )
             }}
           </Query>
         ))}
---
+++ web/src/components/templates/DCCAElectionResult.js
@@ -0,0 +1,137 @@
+import React from 'react'
+import styled from 'styled-components'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import { Tag } from '../atoms/Tag'
+import { PeopleAvatar } from '../atoms/Avatar'
+import { UnstyledNavLink } from '../atoms/Link'
+import Columns from 'components/atoms/Columns'
+import Rows from 'components/atoms/Rows'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import { COLORS } from 'ui/theme'
+
+import {
+  getColorFromPoliticalAffiliation,
+  getCouncillorMeta,
+  getColorFromCamp,
+} from 'utils/helper'
+
+const CandiateBox = styled(Box)`
+  && {
+    display: flex;
+    margin: 8px 0 8px;
+  }
+`
+
+const CandidateAvatar = styled(Columns)`
+  && {
+    position: relative;
+    width: auto;
+    margin-right: 16px;
+  }
+`
+
+const CandidateNumber = styled(Box)`
+  && {
+    position: absolute;
+    top: 40px;
+    left: 3px;
+    border-radius: 50%;
+    font-size: 10px;
+    font-weight: 700;
+    width: ${props => props.dimension};
+    height: ${props => props.dimension};
+    background-color: ${props => COLORS.camp[props.camp].background};
+    color: ${props => COLORS.camp[props.camp].text};
+    text-align: center;
+  }
+`
+
+const CandidateName = styled(Columns)`
+  && {
+    width: auto;
+    min-width: 120px;
+  }
+`
+
+const VotePercentageBar = styled(LinearProgress)`
+  && {
+    background: transparent;
+    width: 100%;
+    height: 16px;
+    .MuiLinearProgress-barColorPrimary {
+      background-color: ${props => COLORS.camp[props.camp].background};
+    }
+  }
+`
+
+const DCCAElectionResult = props => {
+  const { candidates } = props
+
+  const IMAGE_HOST_URI =
+    process.env.REACT_APP_HOST_URI || 'https://hkvoteguide.io'
+
+  const sortedCandidates = candidates.sort((a, b) => b.votes - a.votes)
+  return (
+    <>
+      {sortedCandidates.map(candidate => (
+        <CandiateBox>
+          <CandidateAvatar>
+            <PeopleAvatar
+              dimension="60px"
+              borderwidth={'3'}
+              camp={getColorFromCamp(candidate.camp)}
+              src={`${IMAGE_HOST_URI}/static/images/avatar/${candidate.person.uuid}.jpg`}
+              imgProps={{
+                onError: e => {
+                  e.target.src =
+                    IMAGE_HOST_URI + '/static/images/avatar/default.png'
+                },
+              }}
+            />
+            {candidate.candidate_number > 0 && (
+              <CandidateNumber
+                dimension="16px"
+                camp={getColorFromCamp(candidate.camp)}
+              >
+                {candidate.candidate_number}
+              </CandidateNumber>
+            )}
+            {/* {candidate.is_won && 'win'} */}
+          </CandidateAvatar>
+          <CandidateName>
+            <Rows>
+              <Typography variant="h5">{candidate.person.name_zh}</Typography>
+            </Rows>
+            <Rows>
+              <Typography variant="h6">
+                {candidate.political_affiliation} （{candidate.camp}）
+              </Typography>
+            </Rows>
+          </CandidateName>
+          <Columns>
+            <Rows>
+              <Typography variant="h5">
+                {candidate.votes}（{candidate.vote_percentage}%）
+              </Typography>
+            </Rows>
+            <Rows>
+              <VotePercentageBar
+                // className={classes.margin}
+                variant="determinate"
+                camp={getColorFromCamp(candidate.camp)}
+                value={
+                  (candidate.vote_percentage /
+                    sortedCandidates[0].vote_percentage) *
+                  100
+                }
+              />
+            </Rows>
+          </Columns>
+        </CandiateBox>
+      ))}
+    </>
+  )
+}
+
+export default DCCAElectionResult
