9a048fa34ce7231966f80b7017954b1ac58d24d0
coolsunwind <brianleung1017@gmail.com>
2019-10-14 01:10:22 +0800

    update disclaimer

--- web/src/components/pages/disclaimer/index.js
+++ web/src/components/pages/disclaimer/index.js
@@ -3,14 +3,15 @@ import styled from 'styled-components'
 import { Typography } from '@material-ui/core'
 import Paper from '@material-ui/core/Paper'
 import { DefaultLink } from 'components/atoms/Link'
-import Rows from 'components/atoms/Rows'
-import Columns from 'components/atoms/Columns'
 
 const Container = styled(Paper)`
   && {
     width: 100%;
     padding: 16px;
     box-shadow: none;
+    p {
+      margin-bottom: 1rem;
+    }
     p,
     li {
       font-size: 14px;
@@ -32,73 +33,57 @@ const DisclaimerPage = props => {
         <p>
           候選人的政治陣營，反映他們對政治議題的看法。一般而言，每人對各項議題可持不同立場，難以用一兩個詞語概括候選人背景。
         </p>
+
+        <Typography variant="h5">陣營分類（2019區議會選舉）</Typography>
+
         <p>
-          為於清晰及準確之間取平衡，本網站將候選人的政治陣營按以下次序，分為「建制」、「民主」及「其他」。
+          本屆區議會選舉的候選人政治陣營取自
+          <DefaultLink target="_blank" href="https://hkfactcheck.io">
+            選區事實處
+          </DefaultLink>
+          ，該網站收集大眾意見，並跟據以下資料綜合判斷：
         </p>
         <ol>
-          <li>
-            如候選人屬主流政黨或組織成員，則以其背景劃分，詳細分類可參考附表。
-          </li>
-          <li>
-            如候選人報稱「獨立」，或漏空政治聯繫，則參考其往績，如曾獲某陣營支持、助選或舉辦活動。
-          </li>
-          <li>
-            至於首度參選者，本網站會嘗試按該名侯選人對反修例運動立場劃分：
-            <br />
-            <br />
-            民主：認同運動，要求政府答應運動「五大訴求」
-            <br />
-            建制：反對運動，支持政府「止暴制亂」
-            <br />
-            其他：未有就運動表態，或同時支持以上兩點
-          </li>
+          <li>最近申報之政治聯繫；</li>
+          <li>過往申報之政治聯繫；</li>
+          <li>該政治聯系於重大政治議題上的取態；</li>
+          <li>相關人士及其於重大政治議題上的取態；</li>
+          <li>本人於重大政治議題上的取態；</li>
+          <li>各媒體提及該人士時所使用的介紹文字；</li>
+          <li>與其他組織的關聯。 遇有爭議個案，一律以「其他/未清楚」取代。</li>
         </ol>
+
         <p>
           如任何人分類有任何意見，歡迎反映或電郵至
-          <DefaultLink href="mailto:chiangsumlui@gmail.com">
+          <DefaultLink target="_blank" href="mailto:hkfactcheck@gmail.com">
+            mailto:hkfactcheck@gmail.com
+          </DefaultLink>
+          。
+        </p>
+
+        <Typography variant="h5">2019之前的陣營分類</Typography>
+
+        <p>
+          至於2019年以前之候選人陣營分類，則取自
+          <DefaultLink
+            target="_blank"
+            href="https://github.com/initiummedia/hk_district_council_election"
+          >
+            端傳媒於2017年整理之檔案
+          </DefaultLink>
+          ，本網站刊載前已盡力確保資料真確性，如有建議或錯漏，歡迎
+          <DefaultLink
+            target="_blank"
+            href="https://forms.gle/irD6tEznWPNda6w59"
+          >
+            反映
+          </DefaultLink>
+          或電郵至
+          <DefaultLink target="_blank" href="mailto:chiangsumlui@gmail.com">
             chiangsumlui@gmail.com
           </DefaultLink>
           ，我們會盡快跟進。
         </p>
-
-        <Typography variant="h5">候選陣營分類</Typography>
-
-        <Rows>
-          <Columns>
-            <p>建制</p>
-            <ul>
-              <li>民建聯</li>
-              <li>工聯會</li>
-              <li>經民聯</li>
-              <li>西九新動力</li>
-              <li>自由黨</li>
-              <li>新民黨</li>
-              <li>新世紀論壇</li>
-              <li>香港社團聯會</li>
-              <li>新界社團聯會</li>
-              <li>九龍社團聯會</li>
-              <li>港九勞工社團聯會</li>
-              <li>香港島各界聯合會</li>
-              <li>社區18</li>
-              <li>公民力量</li>
-              <li>公屋聯會</li>
-            </ul>
-            <p>民主</p>
-            <ul>
-              <li>民主黨</li>
-              <li>公民黨</li>
-              <li>民協</li>
-              <li>街工</li>
-              <li>新民主同盟</li>
-              <li>工黨</li>
-              <li>職工盟</li>
-              <li>社民連</li>
-              <li>人民力量</li>
-              <li>支聯會</li>
-              <li>民主動力</li>
-            </ul>
-          </Columns>
-        </Rows>
       </Container>
     </>
   )
