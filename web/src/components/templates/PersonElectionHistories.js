8d197d5d8d49571e625f770c3093322bc411520f
coolsunwind <brianleung1017@gmail.com>
2019-10-16 01:50:59 +0800

    fix election result logic

--- web/src/components/templates/PersonElectionHistories.js
+++ web/src/components/templates/PersonElectionHistories.js
@@ -61,17 +61,15 @@ const PersonElectionHistories = props => {
                   m.camp
                 }）`}</Typography>
               </Grid>
-              {m.votes > 0 && (
-                <Grid item xs={3}>
-                  {m.is_won ? (
-                    <SuccessText>
-                      {m.votes > 0 ? `${m.votes}票` : '自動當選'}
-                    </SuccessText>
-                  ) : (
-                    <FailureText>{`${m.votes}票`}</FailureText>
-                  )}
-                </Grid>
-              )}
+              <Grid item xs={3}>
+                {m.is_won ? (
+                  <SuccessText>
+                    {m.votes > 0 ? `${m.votes}票` : '自動當選'}
+                  </SuccessText>
+                ) : (
+                  <FailureText>{m.votes > 0 && `${m.votes}票`}</FailureText>
+                )}
+              </Grid>
             </Grid>
             <Grid container>
               <Grid item xs={4}>
