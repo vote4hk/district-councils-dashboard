49288dc82d43abee945ffc6e65e5e7c49085899c
wingkwong <wingkwong.code@gmail.com>
2019-10-19 14:58:42 +0800

    Fix missing word for d.count = 1

--- web/src/components/atoms/charts/StackedNormalizedHorizontalBarChart.js
+++ web/src/components/atoms/charts/StackedNormalizedHorizontalBarChart.js
@@ -294,9 +294,7 @@ export default props => {
         .data(d => d)
         .join('text')
         .text(function(d, i, groups, f) {
-          return d.count === 0
-            ? ''
-            : d.count + (d.count / d.data.total > 0.1 ? '席' : '')
+          return d.count === 0 ? '' : `${d.count}席`
         })
         .attr('opacity', 0)
         .attr('x', d => {
