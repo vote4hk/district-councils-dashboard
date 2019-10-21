6f0d28409503e259367637c9046cb86e2c6d6244
Nandiheath <nandi0315@gmail.com>
2019-10-21 23:13:00 +0800

    update profile meta

--- ssr/pages/profile/[name]/[id].js
+++ ssr/pages/profile/[name]/[id].js
@@ -11,8 +11,16 @@ query fetch_user($uuid: uuid!){
   dcd_people(where:{uuid: {_eq: $uuid}}) {
     name_zh
     name_en
-    candidates {
+    candidates(order_by:{
+      year: desc
+    }) {
       cacode
+      constituency {
+        name_zh
+        district {
+          dc_name_zh
+        }
+      }
       occupation
       political_affiliation
     }
@@ -23,9 +31,6 @@ const Profile = () => {
   const router = useRouter()
   const { id, name } = router.query
 
-  console.log(id);
-  console.log(name);
-
   const url = `https://vote4.hk/profile/${name}/${id}`
 
   const { loading, error, data } = useQuery(QUERY_FETCH_PROFILE, {
@@ -35,18 +40,26 @@ const Profile = () => {
   })
 
   if (!loading) {
+    const person = data.dcd_people.length > 0 ? data.dcd_people[0] : {}
+    const displayName = person.name_zh || person.name_en
+    const candidate = person.candidates ? person.candidates[0] || {} : {}
+    const description = `${candidate.constituency.district.dc_name_zh} | ${candidate.constituency.name_zh} | ${candidate.political_affiliation} `
     return (
       <div>
         <Head>
           <title>Vote4HK - {name}</title>
           <link rel='icon' href='/favicon.ico' />
-          <meta property="og:title" content="2019區議會選舉 | 投票指南 | 候選人資料 | 選區分界地圖 | 選情數據分析 " />
-          <meta property="og:description" content="2019區議會選舉於11月24日投票，選出18區452名區議員，提供區選最新消息，輸入地址或搜尋候選人，了解身處選區的背景資料丶候選人政綱及表現。" />
+          <meta property="og:title" content={`${displayName} | 2019區議會選舉 | 投票指南 | 候選人資料 | 選區分界地圖 | 選情數據分析`} />
+          <meta property="og:description" content={`${description}| 2019區議會選舉於11月24日投票，選出18區452名區議員，提供區選最新消息，輸入地址或搜尋候選人，了解身處選區的背景資料丶候選人政綱及表現。`} />
           <meta property="og:type" content="article" />
           <meta property="og:url" content={url} />
           <meta property="article:section" content="政情" />
-          <meta property="article:tag" content="政治, 立法會, 特首, 林鄭月娥, 議員, 選舉, 行政會議, 區議會, 撥款, 委員會, 林鄭月娥, 特首, 候任特首, 特區政府, 高官, 問責官員, 班子, 內閣, 司長, 局長" />
+          <meta property="article:tag" content={`${displayName}, 政治, 立法會, 特首, 林鄭月娥, 議員, 選舉, 行政會議, 區議會, 撥款, 委員會, 林鄭月娥, 特首, 候任特首, 特區政府, 高官, 問責官員, 班子, 內閣, 司長, 局長`} />
         </Head>
+        <div>
+          {displayName}
+        </div>
+
       </div>
     )
   } else {
