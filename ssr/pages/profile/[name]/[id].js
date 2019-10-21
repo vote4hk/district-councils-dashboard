5341a6524576112e6b29abac2da58517f6d26519
Nandiheath <nandi0315@gmail.com>
2019-10-21 23:14:30 +0800

    fix image

--- ssr/pages/profile/[name]/[id].js
+++ ssr/pages/profile/[name]/[id].js
@@ -51,7 +51,10 @@ const Profile = () => {
           <link rel='icon' href='/favicon.ico' />
           <meta property="og:title" content={`${displayName} | 2019區議會選舉 | 投票指南 | 候選人資料 | 選區分界地圖 | 選情數據分析`} />
           <meta property="og:description" content={`${description}| 2019區議會選舉於11月24日投票，選出18區452名區議員，提供區選最新消息，輸入地址或搜尋候選人，了解身處選區的背景資料丶候選人政綱及表現。`} />
-          <meta property="og:type" content="article" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={`https://vote4.hk/static/images/avatar/${id}.jpg`} />
+          <meta property="og:image:width" content="770" />
+          <meta property="og:image:height" content="980" />
           <meta property="og:url" content={url} />
           <meta property="article:section" content="政情" />
           <meta property="article:tag" content={`${displayName}, 政治, 立法會, 特首, 林鄭月娥, 議員, 選舉, 行政會議, 區議會, 撥款, 委員會, 林鄭月娥, 特首, 候任特首, 特區政府, 高官, 問責官員, 班子, 內閣, 司長, 局長`} />
