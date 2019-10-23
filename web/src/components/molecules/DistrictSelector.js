16a1643eb3c5d2324ceb0a5014b48f14ebe7b115
coolsunwind <brianleung1017@gmail.com>
2019-10-24 01:15:52 +0800

    fix lint

--- web/src/components/molecules/DistrictSelector.js
+++ web/src/components/molecules/DistrictSelector.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Button, Box } from '@material-ui/core'
+import { Typography, Button } from '@material-ui/core'
 import styled from 'styled-components'
 import {
   ExpansionPanel,
