d76bdd467fa6c673c687f7aefe9da3d8983f59eb
coolsunwind <brianleung1017@gmail.com>
2019-10-29 14:15:12 +0800

    [personHighlight] minor UI enhancement

--- web/src/components/pages/profile/index.js
+++ web/src/components/pages/profile/index.js
@@ -144,6 +144,7 @@ const FacebookPageButton = styled(UnstyledLink)`
 const PersonHighlightContainer = styled(FlexRowContainer)`
   && {
     padding: 16px;
+    text-align: left;
   }
 `
 const BreadcrumbsContainer = styled(Box)`
@@ -314,14 +315,14 @@ class ProfilePage extends Component {
           }
 
           personHighlight.push({
-            xs: 5,
+            xs: 6,
             title: '相關組織 ',
             tips: '候選人或議員的所屬政黨或社區組織，來源綜合媒體報道',
             text: person.related_organization || '-',
           })
 
           personHighlight.push({
-            xs: 5,
+            xs: 4,
             title: '職業 ',
             tips:
               '候選人：取自最近選舉的候選人簡介<br />議員：取自區議會網頁<br />來源綜合媒體報道',
