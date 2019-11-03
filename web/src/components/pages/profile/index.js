4694efc5c59ce56a60d24a144ce7d340edb5801c
coolsunwind <brianleung1017@gmail.com>
2019-11-04 02:29:36 +0800

    add avatar

--- web/src/components/pages/profile/index.js
+++ web/src/components/pages/profile/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 import Box from '@material-ui/core/Box'
 import styled from 'styled-components'
 import { Typography, Grid, Breadcrumbs, Avatar } from '@material-ui/core'
