59a53df94028fd085af8a1e9dcced70c8cb4ebcf
Tony Yip <tony@tonyhhyip.me>
2019-11-21 00:03:14 +0800

    Add Sentry

--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import * as Sentry from '@sentry/browser'
+import ErrorBoundary from './ErrorBoundary'
 import App from './App'
 import './i18n'
 import './index.css'
 
+Sentry.init({ dsn: process.env.SENTRY_DSN })
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ErrorBoundary>
+    <Router>
+      <App />
+    </Router>
+  </ErrorBoundary>,
   document.getElementById('root')
 )
