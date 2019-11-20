7f29ba49eae55dc92d1a1c9b142a4f650a2552c1
gayaugayau <gayaugayau@protonmail.com>
2019-11-20 21:06:26 +0800

    Tuning based on Google Search Console

--- ssr/lib/locale/zh.js
+++ ssr/lib/locale/zh.js
@@ -4,7 +4,7 @@ const zh = {
   formatSiteName: () => '2019區議會選舉｜Vote4.hk 投票指南',
   formatTitle: (prefix) => `${prefix ? prefix + '｜' : '' }2019區議會選舉｜Vote4.hk 投票指南`,
   formatDescription: () => `2019區議會選舉指南，全港最強選區、候選人名單搜尋器，提供提名、歷屆區議會選舉結果、數據分析、候選人言論及會議出席率。`,
-  formatKeyword: (prefix) => `${prefix ? prefix + ', ' : '' }vote4hk, vote4, 投票指南, 區議會選舉, 區議會, 區選, 選舉, 2019 dc, district council election, 掌心雷, 候選人, 選區, 分界, 地圖, 選情, 數據, 分析, 蘋果, hk01, 立場, scmp, 建制派, 民主派`,
+  formatKeyword: (prefix) => `${prefix ? prefix + ', ' : '' }vote4hk, vote4, 投票指南, 區議會選舉, 區議會, 區選, 選舉, 黃藍, 2019 dc, district council election, 掌心雷, 候選人, 選區, 分界, 地圖, 選情, 數據, 分析, 蘋果, hk01, 立場, scmp, 橙新聞, 建制派, 民主派`,
   formatImageUrl: () => 'https://vote4.hk/og-image.png',
   formatArticleSection: () => '候選人資料｜選區分界地圖｜選情數據分析',
 
